276c7db267892a67021d1a34ad837f7ef2f14245
ashkanvafaee <ashkanvafaee@utexas.edu>
2017-03-22 22:28:04 -0500

    Update neighbors.java

--- neighbors.java
+++ neighbors.java
@@ -1,16 +1,4 @@
-/* WORD LADDER Main.java
- * EE422C Project 3 submission by
- * Replace <...> with your actual data.
- * Ashkan Vafaee
- * av28837
- * 16238
- * Kevin Chau
- * kc28535
- * 16238
- * Slip days used: <0>
- * Git URL:https://github.com/ashkanvafaee/EE422C
- * Spring 2017
- */
+// Used to store valid pegs of word ladder
 package assignment3;
 
 public class neighbors {
